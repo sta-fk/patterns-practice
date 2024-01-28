b7c42ca4ad6cc14bd4e9b7078c99fc5b74ab299c
Anastasiia <anastasia.filipchuk@branderstudio.com>
2024-01-28 22:50:24 +0200

    DPP-2: Php autoload

---
+++ src/Output/OutputControl.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Output;
+
+class OutputControl
+{
+    public function executeHelloWorld(): void
+    {
+        echo '<h1>Hello world</h1>';
+    }
+}
